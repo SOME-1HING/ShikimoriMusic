5db737577ed3e908d64b690b5add12f615d117cb
SOME1HING <yashprakash2005@gmail.com>
2022-08-25 14:48:16 +0530

    Update stats.py

--- ShikimoriMusic/plugins/stats.py
+++ ShikimoriMusic/plugins/stats.py
@@ -39,33 +39,36 @@ def get_readable_time(seconds: int) -> str:
 
 @pbot.on_message(command("stats"))
 async def stats(_, message: Message):
-    uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-    botuptime = get_readable_time((time.time() - starttime))
-    status = "**「❍」Sʏsᴛᴇᴍ Sᴛᴀᴛɪsᴛɪᴄs「❍」**\n\n✦"
-    status += f"**┈➤ Sʏsᴛᴇᴍ Sᴛᴀʀᴛ Tɪᴍᴇ: ** `{str(uptime)}`\n✦"
-    uname = platform.uname()
-    status += f"**┈➤ Sʏsᴛᴇᴍ:** `{str(uname.system)}`\n✦"
-    status += f"**┈➤ Nᴏᴅᴇ Nᴀᴍᴇ:** `{(str(uname.node))}`\n✦"
-    status += f"**┈➤ Rᴇʟᴇᴀsᴇ:** `{(str(uname.release))}`\n✦"
-    status += f"**┈➤ Mᴀᴄʜɪɴᴇ:** `{(str(uname.machine))}`\n✦"
-    mem = virtual_memory()
-    cpu = cpu_percent()
-    disk = disk_usage("/")
-    status += f"**┈➤ Cᴘᴜ:** `{str(cpu)} %`\n✦"
-    status += f"**┈➤ Rᴀᴍ:** `{str(mem[2])} %`\n✦"
-    status += f"**┈➤ Sᴛᴏʀᴀɢᴇ:** `{str(disk[3])} %`\n\n✦"
-    status += f"**┈➤ Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ:** `{python_version()}`\n✦"
-    status += f"**┈➤ Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ:** `{str(pyrover)}`\n✦"
-    status += f"**┈➤ Uᴘᴛɪᴍᴇ:** `{str(botuptime)}`\n"
+    if message.from_user.id in SUDO_USERS:
+        uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+        botuptime = get_readable_time((time.time() - starttime))
+        status = "**「❍」Sʏsᴛᴇᴍ Sᴛᴀᴛɪsᴛɪᴄs「❍」**\n\n✦"
+        status += f"**┈➤ Sʏsᴛᴇᴍ Sᴛᴀʀᴛ Tɪᴍᴇ: ** `{str(uptime)}`\n✦"
+        uname = platform.uname()
+        status += f"**┈➤ Sʏsᴛᴇᴍ:** `{str(uname.system)}`\n✦"
+        status += f"**┈➤ Nᴏᴅᴇ Nᴀᴍᴇ:** `{(str(uname.node))}`\n✦"
+        status += f"**┈➤ Rᴇʟᴇᴀsᴇ:** `{(str(uname.release))}`\n✦"
+        status += f"**┈➤ Mᴀᴄʜɪɴᴇ:** `{(str(uname.machine))}`\n✦"
+        mem = virtual_memory()
+        cpu = cpu_percent()
+        disk = disk_usage("/")
+        status += f"**┈➤ Cᴘᴜ:** `{str(cpu)} %`\n✦"
+        status += f"**┈➤ Rᴀᴍ:** `{str(mem[2])} %`\n✦"
+        status += f"**┈➤ Sᴛᴏʀᴀɢᴇ:** `{str(disk[3])} %`\n\n✦"
+        status += f"**┈➤ Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ:** `{python_version()}`\n✦"
+        status += f"**┈➤ Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ:** `{str(pyrover)}`\n✦"
+        status += f"**┈➤ Uᴘᴛɪᴍᴇ:** `{str(botuptime)}`\n"
 
-    await message.reply_text(
-        status
-        + "\n\n**「✪」Bᴏᴛ Sᴛᴀᴛɪsᴛɪᴄs「✪」**:\n\n✦"
-        + f"**┈➤ Served Chats:** `{len(get_served_chats())}`\n✦" 
-        + f"**┈➤ Served Users:** `{len(get_served_users())}` \n✦"
-        + f"**┈➤ Active VC:** `{len(get_active_chats())}`\n"
+        await message.reply_text(
+            status
+            + "\n\n**「✪」Bᴏᴛ Sᴛᴀᴛɪsᴛɪᴄs「✪」**:\n\n✦"
+            + f"**┈➤ Served Chats:** `{len(get_served_chats())}`\n✦" 
+            + f"**┈➤ Served Users:** `{len(get_served_users())}` \n✦"
+            + f"**┈➤ Active VC:** `{len(get_active_chats())}`\n"
 
-        + f"\n\n    [✦ ꜱᴜᴘᴘᴏʀᴛ ✦](https://t.me/{SUPPORT_CHAT})\n"
-        + " ┗━━✦❘༻           ༺❘✦━━┛",
-        parse_mode="markdown",
-    )
+            + f"\n\n    [✦ ꜱᴜᴘᴘᴏʀᴛ ✦](https://t.me/{SUPPORT_CHAT})\n"
+            + " ┗━━✦❘༻           ༺❘✦━━┛",
+            parse_mode="markdown",
+        )
+    else:
+        await message.reply_text("This is SUDO restricted command.")
