245e7e9cb0f109eab45db3187ff15fd10a99f781
SOME1HING <yashprakash2005@gmail.com>
2022-09-21 16:20:49 +0530

    Merge branch 'main' of https://github.com/SOME-1HING/ShikimoriMusic

--- ShikimoriMusic/vars.py
+++ ShikimoriMusic/vars.py
@@ -20,7 +20,7 @@ HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 DURATION_LIMIT = int(os.environ.get("DURATION_LIMIT", "600"))
 CMD_MUSIC = list(os.environ.get("CMD_MUSIC", "/ !").split())
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001504201905")
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 
 
 SUDO_USERS = (OWNER_ID, 5499316076)
